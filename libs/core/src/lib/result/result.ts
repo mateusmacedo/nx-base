3a2a83107df1f120992d362246187f7683debf18
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-10-19 06:00:05 -0300

    feat(result): :sparkles: add result type definitions
    Introduce new type aliases ResultOk, ResultFail, and ResultOr to improve type clarity and usage.

--- libs/core/src/lib/result/result.ts
+++ libs/core/src/lib/result/result.ts
@@ -1,54 +1,58 @@
 export class Result<T, E> {
-  private constructor(private readonly _value?: T, private readonly _error?: E) {}
+  private constructor(private readonly _value?: T, private readonly _error?: E) { }
 
   get isSuccess(): boolean {
-    return this._error === undefined;
+    return this._error === undefined
   }
 
   get isFailure(): boolean {
-    return !this.isSuccess;
+    return !this.isSuccess
   }
 
   get value(): T {
     if (!this.isSuccess) {
-      throw new Error('Cannot get the value of a failed result.');
+      throw new Error('Cannot get the value of a failed result.')
     }
-    return this._value as T;
+    return this._value as T
   }
 
   get error(): E {
     if (this.isSuccess) {
-      throw new Error('Cannot get the error of a successful result.');
+      throw new Error('Cannot get the error of a successful result.')
     }
-    return this._error as E;
+    return this._error as E
   }
 
   public static ok<T, E = never>(value: T): Result<T, E> {
-    return new Result<T, E>(value);
+    return new Result<T, E>(value)
   }
 
   public static fail<T = never, E = any>(error: E): Result<T, E> {
-    return new Result<T, E>(undefined, error);
+    return new Result<T, E>(undefined, error)
   }
 
   public async andThen<U>(func: (value: T) => Promise<Result<U, E>>): Promise<Result<U, E>> {
     if (this.isFailure) {
-      return Result.fail<U, E>(this.error);
+      return Result.fail<U, E>(this.error)
     }
-    return await func(this.value as T);
+    return await func(this.value as T)
   }
 
   public onFailure(func: (error: E) => void): Result<T, E> {
     if (this.isFailure) {
-      func(this.error as E);
+      func(this.error as E)
     }
-    return this;
+    return this
   }
 
   public onSuccess(func: (value: T) => void): Result<T, E> {
     if (this.isSuccess) {
-      func(this.value as T);
+      func(this.value as T)
     }
-    return this;
+    return this
   }
 }
+
+export type ResultOk<T> = Result<T, never>
+export type ResultFail<E> = Result<never, E>
+export type ResultOr<T, E> = Result<T, E>
