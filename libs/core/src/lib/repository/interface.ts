55749cf886a299af77170169a18a21d60dd6d578
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-10-19 04:15:48 -0300

    refactor(repository): :hammer: unify result handling in interfaces
    Standardize response handling across repository interfaces by returning `Result` types. This enhances error handling consistency and improves code reliability, making the interfaces more robust and easier to work with when dealing with potential errors in data operations.

--- libs/core/src/lib/repository/interface.ts
+++ libs/core/src/lib/repository/interface.ts
@@ -1,17 +1,19 @@
+import { Result } from '../result/result'
+
 export interface IFindById<T, ID> {
-  findById(id: ID): Promise<T | null>
+  findById(id: ID): Promise<Result<T, Error>>
 }
 
 export interface IFindAll<T> {
-  findAll(): Promise<T[]>
+  findAll(): Promise<Result<T[], Error>>
 }
 
 export interface ISave<T> {
-  save(entity: T): Promise<void>
+  save(entity: T): Promise<Result<T, Error>>
 }
 
 export interface IDelete<ID> {
-  delete(id: ID): Promise<void>
+  delete(id: ID): Promise<Result<void, Error>>
 }
 
 export interface IRepository<T, ID> extends IFindById<T, ID>, IFindAll<T>, ISave<T>, IDelete<ID> {}
